d45b19e02ebccc6ee273588dacfe815fa2a6b917
Ben Hopkins <bjhops17@gmail.com>
2021-01-17 12:01:55 -0800

    added boiler plate and packages

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const path = require('path')
+const fs = require("fs")
+
+const PORT = process.env.PORT || 8080
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+///////////////////////////////////////////////////////
+
+app.get('/', function(request, response){
+    response.sendFile(path.join(__dirname, "/index.html"))
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+  })
